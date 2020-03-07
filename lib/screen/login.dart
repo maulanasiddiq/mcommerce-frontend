1287ec4e61c64f2cc7397415db958332265d5eb9
Bayu Adi Prasetiyo <bayu@leson.id>
2020-03-07 17:20:33 +0700

    integrate login with back end

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:mcommerce/screen/menu.dart';
+import 'package:mcommerce/screen/menu/home.dart';
 import 'package:mcommerce/screen/register.dart';
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
   @override
@@ -8,6 +11,81 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+   bool visible = false ;
+ 
+  // Getting value from TextField widget.
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+ 
+Future<http.Response> userLogin() async{
+ 
+  // Showing CircularProgressIndicator.
+  setState(() {
+  visible = true ; 
+  });
+ 
+  // Getting value from Controller
+  String email = emailController.text;
+  String password = passwordController.text;
+ 
+  // SERVER LOGIN API URL
+  var url = 'http://localhost:3000/auth/get/login';
+ 
+  // Store all data with Param Name.
+  var data = {"email": email, "password" : password};
+ 
+  // Starting Web API Call.
+  var response = await http.post(url, body: data);
+ 
+  // Getting Server response into variable.
+  var message = response.body;
+  print(message);
+  // If the Response Message is Matched.
+  if(response.statusCode == 200)
+  {
+    print("berhasil");
+    // // Hiding the CircularProgressIndicator.
+    //   setState(() {
+    //   visible = false; 
+    //   });
+ 
+    // // Navigate to Profile Screen & Sending Email to Next Screen.
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Home())
+      );
+  }else{
+ 
+    // // If Email or Password did not Matched.
+    // // Hiding the CircularProgressIndicator.
+    // setState(() {
+    //   visible = false; 
+    //   });
+ 
+    // // Showing Alert Dialog with Response JSON Message.
+    // showDialog(
+    // context: context,
+    // builder: (BuildContext context) {
+    //   return AlertDialog(
+    //     title: new Text(message),
+    //     actions: <Widget>[
+    //       FlatButton(
+    //         child: new Text("OK"),
+    //         onPressed: () {
+    //           Navigator.of(context).pop();
+    //         },
+    //       ),
+    //     ],
+    //   );
+    // },
+    // );
+    print("gagal");
+    }
+ 
+}
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +109,7 @@ class _LoginState extends State<Login> {
             Padding(
               padding: EdgeInsets.only(left: 20, right: 20),
               child: TextField(
+                controller: emailController,
                   decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Email',
@@ -42,6 +121,7 @@ class _LoginState extends State<Login> {
             Padding(
               padding: EdgeInsets.only(left: 20, right: 20),
               child: TextField(
+                  controller: passwordController,
                   obscureText: true,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
@@ -58,10 +138,7 @@ class _LoginState extends State<Login> {
                   height: 50,
                   child: RaisedButton(
                     color: Colors.teal,
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Menu()));
-                    },
+                    onPressed: userLogin,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10)),
                     child: Text(
@@ -75,6 +152,13 @@ class _LoginState extends State<Login> {
                 ),
               ),
             ),
+            Visibility(
+          visible: visible, 
+          child: Container(
+            margin: EdgeInsets.only(bottom: 30),
+            child: CircularProgressIndicator()
+            )
+          ),
             Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
               Text("Belum punya akun?"),
               SizedBox(
